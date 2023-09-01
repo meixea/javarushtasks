4be591b557a8d9a4b9f7b4dc80ea332dc1b306b8
Mikhail <mixael@bk.ru>
2023-09-01 11:03:37 +0600

    Java Core Task 10-10-10 done. Refactored with multithreading.

--- 2.JavaCore/src/com/javarush/task/task20/task2025/SolTest.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2025/SolTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class SolTest {
 
@@ -19,10 +20,14 @@ public class SolTest {
         test_digits2 = new int[20];
     }
     @Test
+    public void pow(){
+        assertEquals(14348907L, Solution.pow(3L, 15));
+    }
+    @Test
     @DisplayName("Decompose right numbers into massive")
     public void decomposeNumber1(){
         long number = 1234567890123456L;
-        int result = Solution.decomposeNumber(number, test_digits);
+        int result = Solution.CalculationUnit.decomposeNumber(number, test_digits);
         assertEquals(16, result);
         for(int i = 16; i >= 1; i-- )
             assertEquals(i % 10, test_digits[16 - i], String.format("Digit index %d is wrong", i-1));
@@ -32,15 +37,32 @@ public class SolTest {
     public void composeNumber() {
 
         int[] digits = {1, 2, 3, 4, 5, 6};
-        long result = Solution.composeNumber(digits, 0, digits.length);
+        long result = Solution.CalculationUnit.composeNumber(digits, 0, digits.length);
         assertEquals(67171L, result);
     }
     @Test
     @DisplayName("Calculating summ with trailing zeroes")
     public void composeNumber2(){
         int[] digits = {0, 0, 0, 5, 6};
-        long result = Solution.composeNumber(digits, 3, digits.length);
+        long result = Solution.CalculationUnit.composeNumber(digits, 3, digits.length);
         assertEquals(61L, result);
+    }
+    @Test
+    public void testCombo(){
+        Solution.results.clear();
+        Solution.CalculationUnit unit = new Solution.CalculationUnit(4, 9, 0);
+        int[] test_digits3 = {0, 0, 0, 1, 3, 5};
+        unit.testCombo(test_digits3, test_digits2, test_digits3.length);
+        assertEquals(1, Solution.results.size(), "Must find only 1 true number");
+        assertEquals(153L, Solution.results.first(), "Wrong number founded");
+    }
+    @Test
+    public void run(){
+        Solution.results.clear();
+        Solution.CalculationUnit unit = new Solution.CalculationUnit(3, 9, 0);
+        unit.run();
+        assertEquals(new HashSet(Arrays.asList(1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 153L, 370L, 371L, 407L)),
+                     Solution.results);
     }
     @Disabled
     public void getNumbers(){
@@ -56,7 +78,7 @@ public class SolTest {
         int[] digits = new int[20];
         long startTime = System.nanoTime();
         for( int i = 0; i < BENCHMARK_TESTS; i++){
-            int result = Solution.decomposeNumber(number, digits);
+            int result = Solution.CalculationUnit.decomposeNumber(number, digits);
         }
         long endTime = System.nanoTime();
         System.out.printf("test time: %d mcs\n", (endTime - startTime)/1000L);
--- 2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Set;
 import java.util.TreeSet;
 
 /* 
@@ -12,17 +11,85 @@ import java.util.TreeSet;
 */
 
 public class Solution {
-    private static final int THREADS_NUMBER = 2;
+    private static final int THREADS_NUMBER = 9;
 
     private static long[][] digitPowers;
     private static final int MAX_DIGITS = 19;
-    private static Set<Long> results = new TreeSet<>();
+    protected static TreeSet<Long> results = new TreeSet<>();
 
     static {
         digitPowers = new long[10][MAX_DIGITS + 1];
         for(int power = 0; power <= MAX_DIGITS; power++)
             for(long number = 0; number < 10L; number++)
                 digitPowers[(int)number][power] = pow(number, power);
+    }
+    protected static class CalculationUnit extends Thread {
+        private int[] digits;
+        private int[] test_digits;
+        private int endDigit;
+        public CalculationUnit(int M, int startDigit, int endDigit){
+            super();
+
+            digits = new int[M];
+            test_digits = new int[M+1];
+            this.endDigit = endDigit;
+
+            for (int i = 0; i < M; i++)
+                digits[i] = startDigit;
+        }
+        @Override
+        public void run(){
+            int M1 = digits.length - 1;
+            while( digits[M1] > endDigit ){
+
+                testCombo(digits, test_digits, digits.length);
+
+                int not_null_index = 0;
+                while( digits[not_null_index] == 0 )
+                    not_null_index++;
+                digits[not_null_index]--;
+                for(int i = 0; i < not_null_index; i++)
+                    digits[i] = digits[not_null_index];
+            }
+        }
+        protected void testCombo(int[] digits, int[] test_digits, int M){
+            int from = 0;
+            long value;
+            while(true){
+                value = composeNumber(digits, from, M);
+                int testM = decomposeNumber(value, test_digits);
+                if(testM == (M - from)){
+                    long value2 = composeNumber(test_digits, 0, testM);
+                    if( value == value2 )
+                        synchronized(Solution.results) {
+                            results.add(value);
+                        }
+                }
+                if(digits[from] > 0)
+                    break;
+                from++;
+            }
+        }
+        protected static long composeNumber(int[] digits, int from, int to){
+
+            int power = to - from;
+            long summ = 0;
+            for (int i = from; i < to; i++) {
+                summ += digitPowers[digits[i]][power];
+                if( summ < 0 )
+                    return 0;
+            }
+            return summ;
+        }
+        protected static int decomposeNumber(long number, int[] digits){
+            int power = 0;
+            while( number > 0 ){
+                digits[power] = (int)(number % 10L);
+                power++;
+                number /= 10L;
+            }
+            return power;
+        }
     }
 
     public static long[] getNumbers(long N) {
@@ -31,25 +98,24 @@ public class Solution {
             return new long[0];
 
         int[] digits = new int[MAX_DIGITS];
-        int[] test_digits = new int[MAX_DIGITS];
 
-        int M = decomposeNumber(N, digits);
-        int M1 = M - 1;
+        int M = CalculationUnit.decomposeNumber(N, digits);
 
-        for( int i = 0; i < M; i++ )
-            digits[i] = 9;
+        List<Thread> threads = new ArrayList<>();
 
-        while( digits[M1] > 0 ){
-
-            testCombo(digits, test_digits, M);
-
-            int not_null_index = 0;
-            while( digits[not_null_index] == 0 )
-                not_null_index++;
-            digits[not_null_index]--;
-            for(int i = 0; i < not_null_index; i++)
-                digits[i] = digits[not_null_index];
+        int full_size = 9;
+        for( int i = THREADS_NUMBER; i > 0; i--){
+            int current_size = full_size / i;
+            Thread thread = new CalculationUnit(M, full_size, full_size - current_size);
+            threads.add(thread);
+            thread.start();
+            full_size -= current_size;
         }
+        try {
+            for (Thread thread : threads)
+                thread.join();
+        }
+        catch( InterruptedException e ){}
 
         long[] result = results.stream()
                 .mapToLong(i -> i)
@@ -57,47 +123,11 @@ public class Solution {
                 .toArray();
         return result;
     }
-    protected static void testCombo(int[] digits, int[] test_digits, int M){
-        int from = 0;
-        long value;
-        while(true){
-            value = composeNumber(digits, from, M);
-            int testM = decomposeNumber(value, test_digits);
-            if(testM == (M - from)){
-                long value2 = composeNumber(test_digits, 0, testM);
-                if( value == value2 )
-                    results.add(value);
-            }
-            if(digits[from] > 0)
-                break;
-            from++;
-        }
-    }
-    private static long pow(long number, int power){
+    protected static long pow(long number, int power){
         long result = 1;
         for (int i = 0; i < power; i++)
             result *= number;
         return result;
-    }
-    protected static long composeNumber(int[] digits, int from, int to){
-
-        int power = to - from;
-        long summ = 0;
-        for (int i = from; i < to; i++) {
-            summ += digitPowers[digits[i]][power];
-            if( summ < 0 )
-                return 0;
-        }
-        return summ;
-    }
-    protected static int decomposeNumber(long number, int[] digits){
-        int power = 0;
-        while( number > 0 ){
-            digits[power] = (int)(number % 10L);
-            power++;
-            number /= 10L;
-        }
-        return power;
     }
 
     public static void main(String[] args) {
